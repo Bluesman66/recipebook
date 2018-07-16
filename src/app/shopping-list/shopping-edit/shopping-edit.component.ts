e3772a52a50ac45c413bcedc40fca8c6ca5c3466
alex.paclin <alex.paclin@gmail.com>
2018-07-16 12:31:18 +0500

    16/7. Resetting the Form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -11,7 +11,7 @@ import { ShoppingListService } from '../shopping-list.service';
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit, OnDestroy {  
+export class ShoppingEditComponent implements OnInit, OnDestroy {
   @ViewChild('f') private slForm: NgForm;
   private subscription: Subscription;
   editMode = false;
@@ -24,8 +24,8 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.subscription = this.slService.startedEditing
       .subscribe((index: number) => {
         this.editedItemIndex = index;
-        this.editMode = true;   
-        this.editedItem = this.slService.getIngredient(index); 
+        this.editMode = true;
+        this.editedItem = this.slService.getIngredient(index);
         this.slForm.setValue({
           name: this.editedItem.name,
           amount: this.editedItem.amount
@@ -37,7 +37,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.subscription.unsubscribe();
   }
 
-  onAddItem(form: NgForm) {
+  onSubmit(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
     if (this.editMode) {
@@ -45,7 +45,8 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     }
     else {
       this.slService.addIngredient(newIngredient);
-    }    
+    }
+    this.editMode = false;
+    form.reset();
   }
-
 }
