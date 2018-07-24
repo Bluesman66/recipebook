9d79cd2c72326b694f51991ef468283699e3e0e0
alex.paclin <alex.paclin@gmail.com>
2018-07-24 17:03:03 +0500

    23/4. Request Configuration and Response

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -22,10 +22,14 @@ export class DataStorageService {
 
   getRecipes() {    
     const token = this.authService.getToken();
-    this.httpClient.get<Recipe[]>('https://ng-recipe-book-27947.firebaseio.com/recipes.json?auth=' + token)
+    this.httpClient.get<Recipe[]>('https://ng-recipe-book-27947.firebaseio.com/recipes.json?auth=' + token, {
+      observe: 'body',
+      responseType: 'json'
+    })
       .pipe(
         map(
-          (recipes) => {            
+          (recipes) => { 
+            console.log(recipes);           
             for (const recipe of recipes) {
               if (!recipe['ingredients']) {
                 recipe['ingredients'] = [];
