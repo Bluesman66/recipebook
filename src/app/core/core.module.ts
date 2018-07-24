45907951eb400ab4801bf761b2e523075751cc48
alex.paclin <alex.paclin@gmail.com>
2018-07-24 19:14:49 +0500

    23/11. Modifying Requests in Interceptors

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -7,6 +7,7 @@ import { AuthService } from '../auth/auth.service';
 import { RecipeService } from '../recipes/recipe.service';
 import { AuthInterceptor } from '../shared/auth.interceptor';
 import { DataStorageService } from '../shared/data-storage.service';
+import { LoggingInterceptor } from '../shared/logging.interceptor';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
@@ -29,7 +30,8 @@ import { HomeComponent } from './home/home.component';
         RecipeService,
         DataStorageService,
         AuthService,
-        { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+        { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: LoggingInterceptor, multi: true }
       ] 
 })
 export class CoreModule {
