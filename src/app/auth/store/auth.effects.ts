e4a74e18654ed3fa0647b8c586d3f6df4e3a6054
Bluesman66 <alex.paclin@gmail.com>
2018-07-27 23:37:46 +0500

    24/28. Adding Auth Signup

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,3 +1,7 @@
+import * as firebase from 'firebase';
+import { from } from 'rxjs';
+import { map, mergeMap, switchMap } from 'rxjs/operators';
+
 import { Injectable } from '@angular/core';
 import { Actions, Effect } from '@ngrx/effects';
 
@@ -7,7 +11,42 @@ import * as AuthActions from './auth.actions';
 export class AuthEffects {
     @Effect()
     authSignup = this.actions$
-        .ofType(AuthActions.TRY_SIGNUP);
+        .ofType(AuthActions.TRY_SIGNUP)
+        .pipe(
+            map(
+                (action: AuthActions.TrySignup) => {
+                    return action.payload;
+                }
+            )
+        ).pipe(
+            switchMap(
+                (authData: { username: string, password: string }) => {
+                    return from(firebase.auth().createUserWithEmailAndPassword(
+                        authData.username, authData.password)
+                    );
+                }
+            )
+        ).pipe(
+            switchMap(
+                () => {
+                    return from(firebase.auth().currentUser.getIdToken());
+                }
+            )
+        ).pipe(
+            mergeMap(
+                (token: string) => {
+                    return [
+                        {
+                            type: AuthActions.SIGNUP
+                        },
+                        {
+                            type: AuthActions.SET_TOKEN,
+                            payload: token
+                        }
+                    ];
+                }
+            )
+        );
 
     constructor(private actions$: Actions) { }
 }
