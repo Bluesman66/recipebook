19971d832c55eba4abe18a1cb6ce433b1cee495c
alex.paclin <alex.paclin@gmail.com>
2018-07-30 12:34:25 +0500

    tslint

--- src/app/shared/logging.interceptor.ts
+++ src/app/shared/logging.interceptor.ts
@@ -4,7 +4,7 @@ import { tap } from 'rxjs/operators';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 
 export class LoggingInterceptor implements HttpInterceptor {
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {        
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(req).pipe(
             tap(
                 event => console.log('Logging interceptor', event)
