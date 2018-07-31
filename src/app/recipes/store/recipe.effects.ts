9f63151f7906bafe03bc2adc3deabbcffbb27fc1
alex.paclin <alex.paclin@gmail.com>
2018-07-31 13:58:18 +0500

    24/45. Recipes Side Effects - Storing Recipes on Server

--- src/app/recipes/store/recipe.effects.ts
+++ src/app/recipes/store/recipe.effects.ts
@@ -1,11 +1,13 @@
-import { map, switchMap } from 'rxjs/operators';
+import { map, switchMap, withLatestFrom } from 'rxjs/operators';
 
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Actions, Effect } from '@ngrx/effects';
+import { Store } from '@ngrx/store';
 
 import { Recipe } from '../recipe.model';
 import * as RecipeActions from './recipe.actions';
+import * as fromRecipe from './recipe.reducers';
 
 @Injectable()
 export class RecipeEffects {
@@ -20,27 +22,39 @@ export class RecipeEffects {
                         observe: 'body',
                         responseType: 'json'
                     })
-                    .pipe(
-                        map(
-                            (recipes) => {
-                                console.log(recipes);
-                                for (const recipe of recipes) {
-                                    if (!recipe['ingredients']) {
-                                        recipe['ingredients'] = [];
+                        .pipe(
+                            map(
+                                (recipes) => {
+                                    console.log(recipes);
+                                    for (const recipe of recipes) {
+                                        if (!recipe['ingredients']) {
+                                            recipe['ingredients'] = [];
+                                        }
                                     }
+                                    return {
+                                        type: RecipeActions.SET_RECIPES,
+                                        payload: recipes
+                                    };
                                 }
-                                return {
-                                    type: RecipeActions.SET_RECIPES,
-                                    payload: recipes
-                                };
-                            }
-                        )
-                    );
+                            )
+                        );
                 }
             )
         );
 
+    @Effect({ dispatch: false })
+    recipeStore = this.actions$
+        .ofType(RecipeActions.STORE_RECIPES)
+        .pipe(
+            withLatestFrom(this.store.select('recipes')),
+            switchMap(([action, state]) => {
+                const req = new HttpRequest('PUT', 'https://ng-recipe-book-27947.firebaseio.com/recipes.json',
+                    state.recipes, { reportProgress: true });
+                return this.httpClient.request(req);
+            })
+        );
 
     constructor(private actions$: Actions,
-        private httpClient: HttpClient) { }
+        private httpClient: HttpClient,
+        private store: Store<fromRecipe.FeatureState>) { }
 }
